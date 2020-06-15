c35f257132e6849c529b7a1a78b4deadcdbe7799
PeterStanger92020362 <psta191@mywhitecliffe.com>
2020-06-15 19:39:01 +1200

    Finished Question 2

--- src/expressionvalidation/ExpressionValidation.java
+++ src/expressionvalidation/ExpressionValidation.java
@@ -5,6 +5,9 @@
  */
 package expressionvalidation;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /**
  *
  * @author Peter
@@ -15,7 +18,47 @@ public class ExpressionValidation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Program Init
+        Scanner sc = new Scanner(System.in);
+        Stack<Character> expStack = new Stack<>();
+
+        System.out.println("Welcome to the expression validating program.\n");     
+
+        System.out.println("Enter your expression:");
+        String expression = sc.nextLine();
+
+        System.out.println("Enter the opening character to validate:");
+        char expStartChar = sc.next().charAt(0);
+
+        System.out.println("Enter the closing character to validate:");
+        char expCloseChar = sc.next().charAt(0);
+        
+        boolean tooManyClosing = false;
+        
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == expStartChar){
+                expStack.push(expression.charAt(i));
+            } else if (expression.charAt(i) == expCloseChar){
+                if (!expStack.isEmpty()){
+                    expStack.pop();
+                } else {
+                    tooManyClosing = true;
+                    break;
+                }
+            }
+            
+        }
+        
+        if (expStack.isEmpty() && tooManyClosing != true){
+            System.out.println("The expression is validated");
+        } else {
+            System.out.println("The expression is incorrect");
+        }
+
+        
     }
+    
+    
     
 }
